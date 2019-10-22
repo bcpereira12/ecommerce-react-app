2e706b4ceb52bc76b337a0e743523c414ee1a753
Bianca Pereira <bcpereira@Bianca.local>
2019-10-21 23:14:24 -0600

    setup redux form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 
+import { reduxForm, Field } from "redux-form";
+
 class Signin extends Component {
   render() {
     return <div className="sign-in">Sign innnn</div>;
   }
 }
+
+Signin = reduxForm({
+  form: "Signin"
+})(Signin);
 
 export default Signin;
