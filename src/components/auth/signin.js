0666a3d03cff3e42ee178509b1177b804c25c625
Bianca Pereira <bcpereira@Bianca.local>
2019-10-21 22:53:18 -0600

    built signin and signup routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Signin extends Component {
+  render() {
+    return <div className="sign-in">Sign innnn</div>;
+  }
+}
+
+export default Signin;
--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 
 class Navbar extends Component {
   render() {
-    return (
-      <div className="navbar">
-        <img src="http://via.placeholder.com/50x50" />
-      </div>
-    );
+    return <div className="navbar"></div>;
   }
 }
 
