1751eb93c1a60168a5a2922d4a999dc43a5d8937
Bianca Pereira <bcpereira@Bianca.local>
2019-10-22 14:47:32 -0600

    built base structure for account components

---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Purchaseistory extends Component {
+  render() {
+    return <div>Purchase History</div>;
+  }
+}
+
+export default Purchasehistory;
