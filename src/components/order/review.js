12b55168a5e60fd5d06abb5750cea5849810d46c
Bianca Pereira <bcpereira@Bianca.local>
2019-10-24 23:07:18 -0600

    built review form buttons

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -4,16 +4,23 @@ import { connect } from "react-redux";
 import * as actions from "../../actions";
 
 import PageTitle from "../pageTitle";
+import ReviewForm from "./reviewForm";
 
 class Review extends Component {
   componentDidMount() {
     this.props.setHeaderLinks([]);
     this.props.setNavbarLinks([]);
   }
+
+  onSubmit = fileds => {
+    console.log(fields);
+  };
+
   render() {
     return (
       <div className="review">
         <PageTitle className="review__page-title" title="Order Review" />
+        <ReviewForm className="review__form" onSubmit={this.onSubmit} />
       </div>
     );
   }
