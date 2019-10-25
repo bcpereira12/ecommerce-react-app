bf284f7f64cfc75ddf15ade19cbdcbfe9f0a7a5b
Bianca Pereira <bcpereira@Bianca.local>
2019-10-24 21:56:41 -0600

    cart animations and cart button in shop component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -5,6 +5,7 @@ import * as actions from "../../actions";
 import ShopSearchBar from "./shopSearchBar";
 import ShopProduct from "./shopProduct";
 import ShopCart from "./shopCart";
+import CartButton from "./cartButton";
 
 class Shop extends Component {
   constructor() {
@@ -42,6 +43,16 @@ class Shop extends Component {
     this.props.filterProductsWithQuery(fields);
   };
 
+  handleAddToCart = () => {
+    if (
+      document.getElementById("shop-cart").classList.contains("cart-hidden")
+    ) {
+      document.getElementById("shop-cart").classList.remove("cart-hidden");
+    } else {
+      document.getElementById("shop-cart").classList.add("cart-hidden");
+    }
+  };
+
   render() {
     // return <ShopCart className="shop__cart" />;
     return (
@@ -54,6 +65,12 @@ class Shop extends Component {
         </div>
 
         {this.state.showCart ? <ShopCart className="shop__cart" /> : ""}
+
+        <CartButton
+          onClick={this.handleAddToCart}
+          className="shop__cart-button"
+          icon="fas fa-cart-plus"
+        />
       </div>
     );
   }
